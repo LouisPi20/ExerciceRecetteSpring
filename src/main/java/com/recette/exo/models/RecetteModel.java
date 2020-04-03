78bbadc53cba721cd0782b3cf4c1b39733b4336e
Pivain <louis--76@hotmail.fr>
2020-04-03 09:26:06 +0200

    First Commit

---
+++ src/main/java/com/recette/exo/models/RecetteModel.java
@@ -0,0 +1,5 @@
+package com.recette.exo.models;
+
+public class RecetteModel {
+
+}
---
+++ src/test/java/com/recette/exo/RecetteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.recette.exo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecetteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
